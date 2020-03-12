c02d0bfeaf9d708de091b6a4b2a8486b6467aaa4
Juan Rojas <juan.rojas@exsis.com.co>
2020-03-12 17:15:04 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BolkiBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
